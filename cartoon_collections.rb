e2ac8c30193ea4b4dd0b7f81f9ef8840aeb2d324
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 04:07:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include? do |cheese|
-    cheese
+  cheese_types.include?(cheddar)
+  cheese_types.include?(gouda)
 end
