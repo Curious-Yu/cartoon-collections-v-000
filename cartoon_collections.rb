9213b7ff93b4db8f9ab6bc4ec6397d856a43e303
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 03:50:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |name, index|
-    i = i + 1
+    index += 1
     puts "#{index}. #{name}"
   end
 end
