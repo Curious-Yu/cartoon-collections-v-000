0941f35f0b7b1b34e50113f4357501e87c68064c
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 04:11:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_the_cheese(list)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < cheese_types.length
-    return cheese_types[i] if list.included?(cheese_types[i])
+    return cheese_types[i] if list.include?(cheese_types[i])
     i = i + 1
   end
 end
